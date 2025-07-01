b0c8c394194c2dead77748b214a8fb096034df51
Naeel <nailkhakimov123@mail.ru>
2025-07-01 21:41:25 +0500

    Добавил картинку на фон сайта

--- header/animations.js
+++ header/animations.js
@@ -3,8 +3,8 @@ const cap = document.querySelector('#cap')
 window.addEventListener("scroll", (event) => {
     const scrollPosY = window.scrollY
     if (scrollPosY > 100) {
-        cap.classList.add('cap_shadow')
+        cap.classList.add('cap_animation')
     } else {
-        cap.classList.remove('cap_shadow')
+        cap.classList.remove('cap_animation')
     }
 })
